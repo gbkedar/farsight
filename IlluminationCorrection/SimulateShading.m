77fd1d42b10546c37a73121cc0ec22f75ebc1ef0
Kedar Grama <gbkedar@gmail.com>
2014-01-22 13:24:18 -0600

    Debug outputs from matlab code for simulating shading

--- IlluminationCorrection/SimulateShading.m
+++ IlluminationCorrection/SimulateShading.m
@@ -1,4 +1,5 @@
 Im = zeros( 2048,2048,100,'uint16' );
+CountIm = zeros( 2048,2048,3,'uint8' );
 lamdas = [ 500, 2000, 10000 ];
 R = rand( 20*20*100, 1 ).*3+0.5.*ones(20*20*100,1);
 R = round(R); R = cast(R,'int32');
@@ -8,14 +9,15 @@ count = 1;
 [X,Y] = meshgrid(-1023:1024, -1023:1024);
 X = abs(X)./1024; Y = abs(Y)./1024;
 c = rand(14,1)-0.5; c = c./sum(abs(c));
-mul = [ 1, 1, -1]; add = [ 0, 5, 5 ]; scale = [100,500,1000];
+count = 1;
+mul = [ 1, 1, -1]; add = [ 200, 200, 200 ]; scale = [100,500,1000];
 surfs = zeros( 2048,2048,3);
 for i=1:3
     surfs(:,:,i) = c(1).*X.^4 + c(2).*X.^3.*Y + c(3).*X.^2.*Y.^2 + ...
                    c(4).*X.*Y.^3 + c(5).*Y.^4 +  ...
          c(6).*X.^3 + c(7).*X.^2.*Y + c(8).*X.*Y.^2 + c(9).*Y.^3 + ...
          c(10).*X.^2 + c(11).*X.*Y + c(12).*Y.^2 + ...
-         mul(i).*(add(i)+c(13)).*X + mul(i).*(add(i)+c(14)).*Y;
+         mul(i).*c(13).*(add(i)+X) + mul(i).*c(14).*(add(i)+Y);
     surfsmin = min(min(surfs(:,:,i)));
     surfs(:,:,i) = surfs(:,:,i) - surfsmin.*ones(size(X,1),size(X,2));
     surfs(:,:,i) = surfs(:,:,i)./max(max(surfs(:,:,i))).*scale(i);
@@ -28,9 +30,12 @@ for i=1:size(Im,3)
         for k=1:128:2048
             RIm = cast( poissrnd( R(count), 128,128 ), 'uint16' );
             Im(j:j+127,k:k+127,i) = RIm + surfs(j:j+127,k:k+127,R(count));
+            CountIm(j:j+127,k:k+127,R(count)) = ...
+                CountIm(j:j+127,k:k+127,R(count))+ones(128,128,'uint8');
             count = count+1;
         end
     end
 end
-WriteImageSeries(Im, 'E:/Kedar/', 'ShadingSimulated', 'tif');
-WriteImageSeries(surfs, 'E:/Kedar/', 'ShadingSurfaces', 'tif');
+WriteImageSeries(Im, '/home/bollocks/sim/', 'ShSim', 'tif');
+WriteImageSeries(surfs, '/home/bollocks/sim/', 'ShSurf', 'tif');
+WriteImageSeries(CountIm, '/home/bollocks/sim/', 'Counts', 'tif');
